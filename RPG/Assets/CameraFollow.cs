e97df4da6705a8dcce365937ec117cb09460f7db
SamuelSemlan <semlan12@live.com>
2018-01-30 18:11:34 +0100

    Add empty project

---
+++ RPG/Assets/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	GameObject target;
+
+	void Start () {
+		target = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	void LateUpdate () {
+		transform.position = target.transform.position;
+	}
+}
