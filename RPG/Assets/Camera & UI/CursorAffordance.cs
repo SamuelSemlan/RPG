d7aa7dc88209a13f6e105a0915c732d0ce3dd8e1
SamuelSemlan <semlan12@live.com>
2018-02-02 23:12:06 +0100

    Gamepad support

--- RPG/Assets/Camera & UI/CursorAffordance.cs
+++ RPG/Assets/Camera & UI/CursorAffordance.cs
@@ -15,7 +15,7 @@ public class CursorAffordance : MonoBehaviour {
 		cameraRaycaster = GetComponent<CameraRaycaster>();
 	}
 	
-	void Update () {
+	void LateUpdate () {
         switch (cameraRaycaster.layerHit)
         {
             case Layer.Walkable:
--- RPG/Assets/Player/PlayerMovement.cs
+++ RPG/Assets/Player/PlayerMovement.cs
@@ -11,22 +11,50 @@ public class PlayerMovement : MonoBehaviour
     ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
     CameraRaycaster cameraRaycaster;
     Vector3 currentClickTarget;
-        
+
+    bool isInDirectMode = false; //TODO make static later
+
     private void Start()
     {
         cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
         m_Character = GetComponent<ThirdPersonCharacter>();
         currentClickTarget = transform.position;
     }
-
-    //TODO fix issue with click to move and WASD conflicting and increasing speed
+    
 
     // Fixed update is called in sync with physics
     private void FixedUpdate()
     {
+        if (Input.GetKeyDown(KeyCode.G)) // TODO add to menus
+        {
+            isInDirectMode = !isInDirectMode; // Toggle mode
+        }
+
+        if (isInDirectMode)
+        {
+            ProcessDirectMovement();
+        }
+        else
+        {
+            ProcessMouseMovement();
+        }
+    }
+
+    private void ProcessDirectMovement()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        // calculate camera relative direction to move:
+        Vector3 m_CamForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 m_Move = v * m_CamForward + h * Camera.main.transform.right;
+        m_Character.Move(m_Move, false, false);
+    }
+
+    private void ProcessMouseMovement()
+    {
         if (Input.GetMouseButton(0))
         {
-            print("Cursor raycast hit layer " + cameraRaycaster.layerHit);
             switch (cameraRaycaster.layerHit)
             {
                 case Layer.Walkable:
@@ -41,7 +69,7 @@ public class PlayerMovement : MonoBehaviour
             }
         }
         var playerToClickpoint = currentClickTarget - transform.position;
-        if(playerToClickpoint.magnitude >= walkMoveStopRadius)
+        if (playerToClickpoint.magnitude >= walkMoveStopRadius)
         {
             m_Character.Move(currentClickTarget - transform.position, false, false);
         }
